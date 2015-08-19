d455b16d4a7a9c6a03e8422f42c631a363e11908
viile <dakgzhu@foxmail.com>
2015-08-20 00:10:49 +0800

    dfa

--- 1.php
+++ 1.php
@@ -1,6 +1,21 @@
 <?php
-    exec("git add * ",$output);
-    exec('git commit -m "dfa"',$output);
-    exec('git push r master',$output);
-//
+function e($cmd){
+    exec($cmd,$output);
     print_r($output);
+}
+for($i=300;$i>0;$i--){
+    $t = rand(1,10);
+    for($m=0;$m<=$t;$m++){
+	    $fl = fopen("/var/www/html/ror/tmp/1","w");
+	    $txt = date("Y-m-d H:i:s",time())." : ".rand(10000,99999)."\n";
+	    fwrite($fl,$txt);
+	    fclose($fl);
+            sleep(1);
+	    e("git add * ");
+	    e('git commit -m "dfa"');
+	    e('git push r master');
+	    sleep(2);
+    }
+    $o = date("m/d/Y",time()-24*60*60);
+    e("date -s ".$o);
+}
